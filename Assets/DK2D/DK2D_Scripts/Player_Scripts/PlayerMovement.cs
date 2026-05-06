0dc246290ff3a80867bc77f0bfd449ab0c850036
Twilli _ <eclipsek24@gmail.com>
2026-05-06 01:37:44 -0500

    Added Collision Objects & Player Rotation

--- Assets/DK2D/DK2D_Scripts/Player_Scripts/PlayerMovement.cs
+++ Assets/DK2D/DK2D_Scripts/Player_Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ namespace SoT.Player
     {
         [SerializeField] InputControls _inputControls;
 
-        float _playerSpeed = 1f;
+        float _playerSpeed = 6f;
 
         private void Update()
         {
@@ -19,14 +19,21 @@ namespace SoT.Player
 
         void Movement()
         {
+            // Movement input from the input controls
+            Vector2 movementInput = _inputControls.movement;
+
             // Horizontal Movement
-            float xMovement = transform.position.x + (_playerSpeed * _inputControls.movement.x * Time.deltaTime);
+            float xMovement = transform.position.x + (_playerSpeed * movementInput.x * Time.deltaTime);
 
             // Vertical Movement
-            float yMovement = transform.position.y + (_playerSpeed * _inputControls.movement.y * Time.deltaTime);
+            float yMovement = transform.position.y + (_playerSpeed * movementInput.y * Time.deltaTime);
 
             // Final movement position of player after horizontal and vertical movements
             transform.position = new Vector3(xMovement, yMovement, 0);
+
+            // Player Orientation
+            if (movementInput != Vector2.zero)
+                transform.up = movementInput;
         }
     }
 }
